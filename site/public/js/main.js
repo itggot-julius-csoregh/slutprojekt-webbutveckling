da84993bbf6d000e4bc9e7f5fdce64223e8d40f4
Julius Csoregh <julius.csoregh@gmail.com>
2018-05-20 21:02:59 +0200

    Added fade effect to container when menu is toggled

--- site/public/js/main.js
+++ site/public/js/main.js
@@ -1,4 +1,7 @@
 function toggleMenu() {
     var element = document.querySelector(".wrapper");
     element.classList.toggle("show-menu");
+
+    var element = document.querySelector(".container");
+    element.classList.toggle("fade");
 }
