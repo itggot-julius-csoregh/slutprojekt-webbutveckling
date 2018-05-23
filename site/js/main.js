549f8adfe1e8e626d5fd81fa5aca6854e63c468e
Julius Csoregh <julius.csoregh@gmail.com>
2018-05-23 16:26:41 +0200

    Menu icon transition added and made layout for text-filled sites

--- site/js/main.js
+++ site/js/main.js
@@ -4,6 +4,12 @@ function toggleMenu() {
 
     var element = document.querySelector(".fade-plane");
     element.classList.toggle("fade");
+
+    var element = document.querySelector(".menu-icon");
+    element.classList.toggle("fade-menu-icon");
+
+    var element = document.querySelector(".close-icon");
+    element.classList.toggle("show-close-icon");
 /* TODO: fix dis
     var element = document.querySelector("body");
     element.classList.toggle("no-scroll"); */
