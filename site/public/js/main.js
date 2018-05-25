8de132b79a6063c9371e8ec81418bd7b01c15be3
Julius Csoregh <julius.csoregh@gmail.com>
2018-05-25 13:09:07 +0200

    Added lorem ipsum to all sites

--- site/public/js/main.js
+++ site/public/js/main.js
@@ -15,11 +15,6 @@ function toggleMenu() {
     element.classList.toggle("no-scroll"); */
 }
 
-function linkPressed(linkNum) {
-    var element = document.querySelector("li:nth-child(" + linkNum + ")");
-    element.classList.toggle("menu-link");
-}
-
 $(document).ready(function () {
     $(".logo").click(function(){
         window.location = $(this).find("a:first").attr("href");
